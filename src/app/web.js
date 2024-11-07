28c9a4fc503542a085def005bd6aff743561d058
milson6389 <audie.milson38@gmail.com>
2024-11-07 20:43:57 +0700

    Feat: Implement CRUD Basic User / Login API

---
+++ src/app/web.js
@@ -0,0 +1,21 @@
+import dotenv from "dotenv";
+import express from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import { corsOptions } from "../config/corsOptions.js";
+import { errorMiddleware } from "../middleware/errorMiddleware.js";
+import { logger } from "../middleware/logger.js";
+import { userRouter } from "../routes/userApi.js";
+
+dotenv.config();
+export const web = express();
+web.use(logger);
+web.use(cors(corsOptions));
+web.use(cookieParser());
+web.use(express.urlencoded({ extended: true }));
+web.use(express.json());
+web.get("/", (req, res) => {
+  res.send("OK!");
+});
+web.use(userRouter);
+web.use(errorMiddleware);
